c9e6edd4991aeaea5512bb9ad819f575c97b0af7
surreal-code <yimina@naver.com>
2022-07-18 14:16:31 +0900

    Updates

---
+++ precache-manifest.64afa9fd23a018ad3a793462c4c3c961.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "526432dfb7c4f7e11b6c",
+    "url": "/react-moive-2/static/css/main.9d1e904e.chunk.css"
+  },
+  {
+    "revision": "526432dfb7c4f7e11b6c",
+    "url": "/react-moive-2/static/js/main.526432df.chunk.js"
+  },
+  {
+    "revision": "933d39bd958e236e8b58",
+    "url": "/react-moive-2/static/js/1.933d39bd.chunk.js"
+  },
+  {
+    "revision": "b4045eef8f7a7e7dd6dd",
+    "url": "/react-moive-2/static/js/runtime~main.b4045eef.js"
+  },
+  {
+    "revision": "2dd647c9d1ffe82976ad748eb423b40e",
+    "url": "/react-moive-2/index.html"
+  }
+];
